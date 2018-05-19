547a1c97d03d843e8249f5f13b85c6053b96fa8e
dudkina_m <dudkina.m.94@gmail.com>
2018-05-20 01:17:21 +0300

    contacts vreadcrumb map side menu

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,28 @@
 $(function () {
   // modalDisplay();
+    if ($("#map").length)
+        initMap();
 });
+
+function initMap() {
+    ymaps.ready(init);
+    var myMap,
+        myPlacemark;
+
+    function init(){
+        myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 7
+        });
+    }
+
+    myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+        hintContent: 'Москва!',
+        balloonContent: 'Столица России'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
 
 function dropdownShow() {
     $(document).ready(function () {
